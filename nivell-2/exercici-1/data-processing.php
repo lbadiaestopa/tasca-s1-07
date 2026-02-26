e24b96806a1f0082b2fe4f8dd80d8a788ee55302
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-02-26 12:17:58 +0100

    feat: add try catch custom exceptions

--- nivell-2/exercici-1/data-processing.php
+++ nivell-2/exercici-1/data-processing.php
@@ -1,69 +1,75 @@
 <?php
 session_start();
 
+class NameException extends Exception {}
+class AgeException extends Exception {}
+class PasswordException extends Exception {}
+
+
 try {
     if (!isset($_POST['name'])) {
-        throw new InvalidArgumentException("Error: Name field does not exist.");
+        throw new NameException("Error: Name field does not exist.");
     }
 
     $name = trim($_POST['name']);
 
     if ($name === '') {
-        throw new InvalidArgumentException("Error: Name field cannot be empty.");
+        throw new NameException("Error: Name field cannot be empty.");
     }
 
     $_SESSION['name'] = $name;
 
     if (!isset($_POST['age'])) {
-        throw new InvalidArgumentException("Error: Age field does not exist.");
+        throw new AgeException("Error: Age field does not exist.");
     }
 
     $rawAge = trim($_POST['age']);
 
     if ($rawAge === '') {
-        throw new InvalidArgumentException("Error: Age field cannot be empty.");
+        throw new AgeException("Error: Age field cannot be empty.");
     }
 
     if (!is_numeric($rawAge)) {
-        throw new InvalidArgumentException("Error: Age must be a number.");
+        throw new AgeException("Error: Age must be a number.");
     }
 
     $age = (int) $rawAge;
 
     if ($age < 0 || $age > 120) {
-        throw new InvalidArgumentException("Error: Age must be between 0 and 120.");
+        throw new AgeException("Error: Age must be between 0 and 120.");
     }
 
     $_SESSION['age'] = $age;
 
     if (!isset($_POST['pass'])) {
-        throw new InvalidArgumentException("Error: Password field does not exist.");
+        throw new AgeException("Error: Password field does not exist.");
     }
 
     $password = $_POST['pass'];
 
     if ($password === '') {
-        throw new InvalidArgumentException("Error: Password cannot be empty.");
+        throw new PasswordException("Error: Password cannot be empty.");
     }
 
     if (strlen($password) < 8) {
-        throw new InvalidArgumentException("Error: Password must be at least 8 characters.");
+        throw new PasswordException("Error: Password must be at least 8 characters.");
     }
 
     if (strpos($password, ' ') !== false) {
-        throw new InvalidArgumentException("Error: Password cannot contain spaces.");
+        throw new NameException("Error: Password cannot contain spaces.");
     }
 
     if (!preg_match('/[a-zA-Z]/', $password)) {
-        throw new InvalidArgumentException("Error: Password must contain at least one letter.");
+        throw new PasswordException("Error: Password must contain at least one letter.");
     }
 
     if (!preg_match('/\d/', $password)) {
-        throw new InvalidArgumentException("Error: Password must contain at least one number.");
+        throw new PasswordException("Error: Password must contain at least one number.");
     }
 
     $_SESSION['pass'] = $password;
-} catch (InvalidArgumentException $e) {
+    
+} catch (NameException | AgeException | PasswordException $e) {
     $error = $e->getMessage();
 }
 
