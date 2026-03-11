16d1960c244b4eee1fb3b3e632d2b755a12ed561
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-03-11 16:45:04 +0100

    fix: move custom exceptions to their own file

---
+++ nivell-2/exercici-1/exceptions/PasswordException.php
@@ -0,0 +1,22 @@
+<?php
+
+class PasswordException extends ValidationException
+{
+    public function __construct(Rule $rule, ?Throwable $previous = null)
+    {
+        parent::__construct(Field::PASS, $rule, $previous);
+    }
+
+    protected function buildMessage(Field $field, Rule $rule): string
+    {
+        return match ($rule) {
+            Rule::MISSING        => 'Password field does not exist.',
+            Rule::EMPTY          => 'Password cannot be empty.',
+            Rule::TOO_SHORT      => 'Password must be at least 8 characters.',
+            Rule::CONTAINS_SPACE => 'Password cannot contain spaces.',
+            Rule::NO_LETTER      => 'Password must contain at least one letter.',
+            Rule::NO_NUMBER      => 'Password must contain at least one number.',
+            default              => 'Invalid password.',
+        };
+    }
+}
