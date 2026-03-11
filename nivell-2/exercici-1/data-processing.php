bb87b62cf4646eebbe1733225c94f8bbff576b40
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-03-11 16:43:22 +0100

    fix: move custom exceptions to their own file

--- nivell-2/exercici-1/data-processing.php
+++ nivell-2/exercici-1/data-processing.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+require_once __DIR__ . '/exceptions/ValidationException.php';
+require_once __DIR__ . '/exceptions/NameException.php';
+require_once __DIR__ . '/exceptions/AgeException.php';
+require_once __DIR__ . '/exceptions/PasswordException.php';
+
 enum Field: string
 {
     case NAME = 'name';
@@ -20,95 +25,6 @@ enum Rule: string
     case CONTAINS_SPACE = 'contains_space';
     case NO_LETTER     = 'no_letter';
     case NO_NUMBER     = 'no_number';
-}
-
-abstract class ValidationException extends Exception
-{
-    protected const HTTP_STATUS = 422;
-
-    protected Field $field;
-    protected Rule  $rule;
-
-    public function __construct(Field $field, Rule $rule, ?Throwable $previous = null)
-    {
-        $this->field = $field;
-        $this->rule  = $rule;
-
-        parent::__construct(
-            $this->buildMessage($field, $rule),
-            self::HTTP_STATUS,
-            $previous
-        );
-    }
-
-    abstract protected function buildMessage(Field $field, Rule $rule): string;
-
-    public function getField(): Field
-    {
-        return $this->field;
-    }
-
-    public function getRule(): Rule
-    {
-        return $this->rule;
-    }
-}
-
-class NameException extends ValidationException
-{
-    public function __construct(Rule $rule, ?Throwable $previous = null)
-    {
-        parent::__construct(Field::NAME, $rule, $previous);
-    }
-
-    protected function buildMessage(Field $field, Rule $rule): string
-    {
-        return match ($rule) {
-            Rule::MISSING => 'Name field does not exist.',
-            Rule::EMPTY   => 'Name cannot be empty.',
-            default       => 'Invalid name.',
-        };
-    }
-}
-
-class AgeException extends ValidationException
-{
-    public function __construct(Rule $rule, ?Throwable $previous = null)
-    {
-        parent::__construct(Field::AGE, $rule, $previous);
-    }
-
-    protected function buildMessage(Field $field, Rule $rule): string
-    {
-        return match ($rule) {
-            Rule::MISSING        => 'Age field does not exist.',
-            Rule::EMPTY          => 'Age cannot be empty.',
-            Rule::HAS_LETTER     => 'Age cannot contain letters.',
-            Rule::OUT_LIMIT      => 'Age must be between 0 and 120.',
-            default              => 'Invalid age.',
-        };
-    }
-}
-
-class PasswordException extends ValidationException
-{
-    public function __construct(Rule $rule, ?Throwable $previous = null)
-    {
-        parent::__construct(Field::PASS, $rule, $previous);
-    }
-
-    protected function buildMessage(Field $field, Rule $rule): string
-    {
-        return match ($rule) {
-            Rule::MISSING        => 'Password field does not exist.',
-            Rule::EMPTY          => 'Password cannot be empty.',
-            Rule::TOO_SHORT      => 'Password must be at least 8 characters.',
-            Rule::CONTAINS_SPACE => 'Password cannot contain spaces.',
-            Rule::NO_LETTER      => 'Password must contain at least one letter.',
-            Rule::NO_NUMBER      => 'Password must contain at least one number.',
-            default              => 'Invalid password.',
-        };
-    }
 }
 
 function validateRequiredField(array $source, string $fieldName): string
@@ -162,8 +78,6 @@ function validatePassword(string $password): string
     return $password;
 }
 
-
-
 try {
     $name = validateRequiredField($_POST, "name");
     $_SESSION['name'] = $name;
@@ -191,7 +105,7 @@ try {
             <p class="error"><?= htmlspecialchars($error) ?></p>
         <?php else : ?>
             <h1>Dades rebudes</h1>
-            <p>Hola, <?= htmlspecialchars($name) ?>! Tens <?= $age ?> anys i la teva contrassenya és <?= $password ?></p>
+            <p>Hola, <?= htmlspecialchars($name) ?>! Tens <?= $age ?> anys i la teva contrasenya és <?= $password ?></p>
         <?php endif; ?>
 
     </div>
